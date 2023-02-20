a61a5cd42cab2a4c9a124912c1adcdbaf5bc453f
Elmo <120378071+ElmoMyllyla@users.noreply.github.com>
2023-02-20 12:09:07 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-
-
-
-
+let nimi ="Matkustaja!"
+let moikkaus ="Moikka "
 let laskuri= 0
+let tallennus = "Kävijät"
+let kutsuttuteksti =document.getElementById("laskuri-el")
+let tallennuskutsu =document.getElementById("tallennus-el")
+let moikkauskutsu= document.getElementById("moikkaus-el")
+moikkauskutsu.innerText= moikkaus + nimi
+
 document.getElementById("Nappula")
 
-
-
-
-function JormanLisaus(){
+function Lisaa(){
    
     laskuri = laskuri + 1
     console.log(laskuri)
@@ -16,9 +17,24 @@ function JormanLisaus(){
     document.getElementById("laskuri-el").innerText = laskuri
 }
 
-function JormanPoisto(){
+function Poistaa(){
    
-    laskuri = laskuri - 7
+    laskuri = laskuri - 1
+    console.log(laskuri)
+    console.log("Miinustus nappulaa painettiin. Hei Hei :(")
+    document.getElementById("laskuri-el").innerText = laskuri
+}
+function tallenna(){
+    console.log(laskuri)
+    let lisays = "-" + laskuri
+    tallennus = tallennus + lisays
+    tallennuskutsu.innerText =tallennus
+    laskuri = laskuri - laskuri
+    document.getElementById ("laskuri-el").innerText=laskuri
+}
+function Poistamonta(){
+   
+    laskuri = laskuri - 5
     console.log(laskuri)
     console.log("Miinustus nappulaa painettiin. Hei Hei :(")
     document.getElementById("laskuri-el").innerText = laskuri
